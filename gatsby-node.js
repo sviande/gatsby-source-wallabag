6a1d7145693a9e9cd0f5fa59609f77843db2935f
Sébastien Viande <sviande@gmail.com>
2019-06-20 20:07:41 +0200

    Init

---
+++ gatsby-node.js
@@ -0,0 +1,87 @@
+const fetch = require("node-fetch");
+const queryString = require("query-string");
+
+exports.sourceNodes = async (
+  { actions, createNodeId, createContentDigest, store },
+  configOptions
+) => {
+  const { createNode, setPluginStatus } = actions;
+
+  let lastFetched = null;
+  if (
+    store.getState().status.plugins &&
+    store.getState().status.plugins["gatsby-source-wallabag"]
+  ) {
+    lastFetched = store.getState().status.plugins["gatsby-source-wallabag"]
+      .lastFetched;
+  }
+
+  const articleToNode = article => {
+    return {
+      ...article,
+      id: createNodeId(article.id),
+      parent: null,
+      children: [],
+      internal: {
+        type: "WallabagArticles",
+        contentDigest: createContentDigest(article)
+      }
+    };
+  };
+
+  // Gatsby adds a configOption that's not needed for this plugin, delete it
+  delete configOptions.plugins;
+
+  const authBody = {
+    grant_type: "password",
+    client_id: configOptions.clientID,
+    client_secret: configOptions.clientSecret,
+    username: configOptions.username,
+    password: configOptions.password
+  };
+
+  const authResponse = await fetch(`${configOptions.URL}/oauth/v2/token`, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: queryString.stringify(authBody)
+  });
+  const auth = await authResponse.json();
+
+  const pageQuery = {
+    sort: "updated",
+    perPage: 50
+  };
+
+  if (lastFetched) {
+    pageQuery.since = Math.round(lastFetched / 1000);
+  }
+
+  lastFetched = new Date();
+
+  const pageResponse = await fetch(
+    `${configOptions.URL}/api/entries.json?${queryString.stringify(pageQuery)}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${auth.access_token}`
+      }
+    }
+  );
+  let entries = await pageResponse.json();
+
+  do {
+    entries._embedded.items.forEach(article =>
+      createNode(articleToNode(article))
+    );
+
+    const response = await fetch(entries._links.next.href, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${auth.access_token}`
+      }
+    });
+    entries = await response.json();
+  } while (entries._links.next);
+
+  setPluginStatus({ lastFetched: lastFetched.getTime() });
+};
