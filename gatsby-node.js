3a6e0454d72b5bafe439e916adc28e9ddfa682a9
Sébastien Viande <sviande@gmail.com>
2021-11-17 20:48:50 +0100

    Reporter: init activity status report

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,27 +2,25 @@ const fetch = require("node-fetch");
 const queryString = require("query-string");
 
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest, store, getNodes },
+  { actions, createNodeId, createContentDigest, store, getNodes, reporter },
   configOptions
 ) => {
   const { createNode, setPluginStatus, touchNode } = actions;
 
   const existingNodes = getNodes().filter(
-    n => n.internal.owner === `gatsby-source-wallabag`
-  )
-  existingNodes.forEach(n => touchNode(n))
+    (n) => n.internal.owner === `gatsby-source-wallabag`
+  );
+  existingNodes.forEach((n) => touchNode(n));
 
-
-  const pluginStatus = store.getState().status.plugins[
-    "gatsby-source-wallabag"
-  ];
+  const pluginStatus =
+    store.getState().status.plugins["gatsby-source-wallabag"];
 
   let lastFetched = null;
   if (pluginStatus) {
     lastFetched = pluginStatus.lastFetched;
   }
 
-  const articleToNode = article => {
+  const articleToNode = (article) => {
     return {
       ...article,
       id: createNodeId(article.id),
@@ -30,8 +28,8 @@ exports.sourceNodes = async (
       children: [],
       internal: {
         type: "WallabagArticles",
-        contentDigest: createContentDigest(article)
-      }
+        contentDigest: createContentDigest(article),
+      },
     };
   };
 
@@ -43,19 +41,19 @@ exports.sourceNodes = async (
     client_id: configOptions.clientID,
     client_secret: configOptions.clientSecret,
     username: configOptions.username,
-    password: configOptions.password
+    password: configOptions.password,
   };
 
   const authResponse = await fetch(`${configOptions.URL}/oauth/v2/token`, {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: queryString.stringify(authBody)
+    body: queryString.stringify(authBody),
   });
   const auth = await authResponse.json();
 
   const pageQuery = {
     sort: "updated",
-    perPage: 500
+    perPage: 500,
   };
 
   if (lastFetched) {
@@ -64,13 +62,16 @@ exports.sourceNodes = async (
 
   lastFetched = new Date();
 
+  let nbEntries = 0;
+  const activity = reporter.activityTimer("Fetching wallabag articles");
+  activity.start();
   const pageResponse = await fetch(
     `${configOptions.URL}/api/entries.json?${queryString.stringify(pageQuery)}`,
     {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${auth.access_token}`
-      }
+        Authorization: `Bearer ${auth.access_token}`,
+      },
     }
   );
 
@@ -81,7 +82,9 @@ exports.sourceNodes = async (
   let entries = await pageResponse.json();
 
   do {
-    entries._embedded.items.forEach(article => {
+    nbEntries += entries._embedded.items.length;
+    activity.setStatus(`number of articles fetched: ${nbEntries}`);
+    entries._embedded.items.forEach((article) => {
       article.created_at = new Date(article.created_at);
       article.updated_at = new Date(article.updated_at);
       article.archived_at = article.archived_at
@@ -96,11 +99,12 @@ exports.sourceNodes = async (
     const response = await fetch(entries._links.next.href, {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${auth.access_token}`
-      }
+        Authorization: `Bearer ${auth.access_token}`,
+      },
     });
     entries = await response.json();
   } while (entries._links.next);
+  activity.end();
 
   setPluginStatus({ lastFetched: lastFetched.getTime() });
   return;
